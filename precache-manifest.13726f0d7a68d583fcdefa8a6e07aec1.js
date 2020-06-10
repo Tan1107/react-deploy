ac36b5b241a7cb125de78b0b8109df3f7af84864
Tan1107 <tannguyen1107.bku@gmail.com>
2020-06-10 14:42:26 +0700

    Updates

---
+++ precache-manifest.13726f0d7a68d583fcdefa8a6e07aec1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5dc348bd10e64b83faa11c7e2ee6a4cd",
+    "url": "/react-deploy/index.html"
+  },
+  {
+    "revision": "8e92e865fb782b6f6f7d",
+    "url": "/react-deploy/static/css/main.ec6f409b.chunk.css"
+  },
+  {
+    "revision": "faad9ad3b806f01381b6",
+    "url": "/react-deploy/static/js/2.26eb4367.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/react-deploy/static/js/2.26eb4367.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e92e865fb782b6f6f7d",
+    "url": "/react-deploy/static/js/main.8f3bf151.chunk.js"
+  },
+  {
+    "revision": "88cc752382718b1d2923",
+    "url": "/react-deploy/static/js/runtime-main.625baed9.js"
+  }
+]);
